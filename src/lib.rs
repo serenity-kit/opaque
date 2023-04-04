f9fefa063559ef9974f88557b2e784bcad0c641e
Stefan Oestreicher <oestef@gmail.com>
2023-04-04 12:52:52 +0200

    Add opaque-ke and related dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 mod utils;
 
+use argon2::Argon2;
+use opaque_ke::ciphersuite::CipherSuite;
+use opaque_ke::rand::rngs::OsRng;
+use opaque_ke::ServerSetup;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -13,7 +17,20 @@ extern "C" {
     fn alert(s: &str);
 }
 
+#[allow(dead_code)]
+struct DefaultCipherSuite;
+
+impl CipherSuite for DefaultCipherSuite {
+    type OprfCs = opaque_ke::Ristretto255;
+    type KeGroup = opaque_ke::Ristretto255;
+    type KeyExchange = opaque_ke::key_exchange::tripledh::TripleDh;
+
+    type Ksf = Argon2<'static>;
+}
+
 #[wasm_bindgen]
 pub fn greet() {
+    let mut rng = OsRng;
+    let server_setup = ServerSetup::<DefaultCipherSuite>::new(&mut rng);
     alert("Hello, opaque im here!");
 }
