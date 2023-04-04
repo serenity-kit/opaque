84d2523325aeab8b89d8b92f78762def5289f154
Stefan Oestreicher <oestef@gmail.com>
2023-04-04 14:20:09 +0200

    Add simple_login example code from opaque-ke

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@ mod utils;
 use argon2::Argon2;
 use opaque_ke::ciphersuite::CipherSuite;
 use opaque_ke::rand::rngs::OsRng;
-use opaque_ke::ServerSetup;
+use opaque_ke::{
+    ClientLogin, ClientLoginFinishParameters, ClientRegistration,
+    ClientRegistrationFinishParameters, CredentialFinalization, CredentialRequest,
+    CredentialResponse, RegistrationRequest, RegistrationResponse, RegistrationUpload, ServerLogin,
+    ServerLoginStartParameters, ServerRegistration, ServerRegistrationLen, ServerSetup,
+};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -15,6 +20,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[allow(dead_code)]
@@ -28,9 +35,123 @@ impl CipherSuite for DefaultCipherSuite {
     type Ksf = Argon2<'static>;
 }
 
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+fn account_registration(
+    server_setup: &ServerSetup<DefaultCipherSuite>,
+    username: &String,
+    password: &String,
+) -> Vec<u8> {
+    let mut client_rng = OsRng;
+    let client_registration_start_result =
+        ClientRegistration::<DefaultCipherSuite>::start(&mut client_rng, password.as_bytes())
+            .unwrap();
+    let registration_request_bytes = client_registration_start_result.message.serialize();
+
+    // Client sends registration_request_bytes to server
+
+    let server_registration_start_result = ServerRegistration::<DefaultCipherSuite>::start(
+        server_setup,
+        RegistrationRequest::deserialize(&registration_request_bytes).unwrap(),
+        username.as_bytes(),
+    )
+    .unwrap();
+    let registration_response_bytes = server_registration_start_result.message.serialize();
+
+    // Server sends registration_response_bytes to client
+
+    let client_finish_registration_result = client_registration_start_result
+        .state
+        .finish(
+            &mut client_rng,
+            password.as_bytes(),
+            RegistrationResponse::deserialize(&registration_response_bytes).unwrap(),
+            ClientRegistrationFinishParameters::default(),
+        )
+        .unwrap();
+    let message_bytes = client_finish_registration_result.message.serialize();
+
+    // Client sends message_bytes to server
+
+    let password_file = ServerRegistration::finish(
+        RegistrationUpload::<DefaultCipherSuite>::deserialize(&message_bytes).unwrap(),
+    );
+    password_file.serialize().to_vec()
+}
+
+fn account_login(
+    server_setup: &ServerSetup<DefaultCipherSuite>,
+    username: &String,
+    password: &String,
+    password_file_bytes: &[u8],
+) -> bool {
+    let mut client_rng = OsRng;
+    let client_login_start_result =
+        ClientLogin::<DefaultCipherSuite>::start(&mut client_rng, password.as_bytes()).unwrap();
+    let credential_request_bytes = client_login_start_result.message.serialize();
+
+    // Client sends credential_request_bytes to server
+
+    let password_file =
+        ServerRegistration::<DefaultCipherSuite>::deserialize(password_file_bytes).unwrap();
+    let mut server_rng = OsRng;
+    let server_login_start_result = ServerLogin::start(
+        &mut server_rng,
+        server_setup,
+        Some(password_file),
+        CredentialRequest::deserialize(&credential_request_bytes).unwrap(),
+        username.as_bytes(),
+        ServerLoginStartParameters::default(),
+    )
+    .unwrap();
+    let credential_response_bytes = server_login_start_result.message.serialize();
+
+    // Server sends credential_response_bytes to client
+
+    let result = client_login_start_result.state.finish(
+        password.as_bytes(),
+        CredentialResponse::deserialize(&credential_response_bytes).unwrap(),
+        ClientLoginFinishParameters::default(),
+    );
+
+    if result.is_err() {
+        // Client-detected login failure
+        return false;
+    }
+    let client_login_finish_result = result.unwrap();
+    let credential_finalization_bytes = client_login_finish_result.message.serialize();
+
+    // Client sends credential_finalization_bytes to server
+
+    let server_login_finish_result = server_login_start_result
+        .state
+        .finish(CredentialFinalization::deserialize(&credential_finalization_bytes).unwrap())
+        .unwrap();
+
+    client_login_finish_result.session_key == server_login_finish_result.session_key
+}
+
 #[wasm_bindgen]
 pub fn greet() {
     let mut rng = OsRng;
     let server_setup = ServerSetup::<DefaultCipherSuite>::new(&mut rng);
-    alert("Hello, opaque im here!");
+
+    let user = String::from("user");
+    let pass = String::from("password");
+
+    let password_file_bytes = account_registration(&server_setup, &user, &pass);
+
+    let login_result = account_login(
+        &server_setup,
+        &user,
+        &String::from("password2"),
+        &password_file_bytes,
+    );
+
+    console_log!("Hello {:?}!", login_result);
+    // alert("Hello, opaque im here!");
 }
