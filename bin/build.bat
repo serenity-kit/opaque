f033a45943d131cc7141d18701c66f9dbb88fbc5
Stefan Oestreicher <oestef@gmail.com>
2023-06-02 13:27:09 +0200

    Simplify build with single shelljs script

--- bin/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rmdir /s /q build
-
-cargo build --target=wasm32-unknown-unknown --release --features wee_alloc
-wasm-bindgen --out-dir=build\wbg_ristretto --target=web --omit-default-module-path target\wasm32-unknown-unknown\release\opaque.wasm
-
-cargo build --target=wasm32-unknown-unknown --release --features wee_alloc,p256
-wasm-bindgen --out-dir=build\wbg_p256 --target=web --omit-default-module-path target\wasm32-unknown-unknown\release\opaque.wasm
--- bin/build.js
+++ bin/build.js
@@ -1,27 +1,95 @@
-const { execFileSync } = require("child_process");
-const { writeFileSync } = require("fs");
-const path = require("path");
+const sh = require("shelljs");
 
-if (process.platform === "win32") {
-  execFileSync(path.join(__dirname, "build.bat"), {
-    stdio: "inherit",
-  });
-} else {
-  execFileSync(path.join(__dirname, "build.sh"), {
-    stdio: "inherit",
-  });
-}
+// throw if a command fails
+sh.config.fatal = true;
 
-const entryModule = `
+const rootPkg = JSON.parse(sh.cat("package.json").toString());
+
+const entryModule = new sh.ShellString(`
 import wasmData from './opaque_bg.wasm'
 import init from './opaque'
 export const ready = init(wasmData())
 export * from './opaque'
 export {default} from './opaque'
-`;
+`);
 
-writeFileSync(
-  path.join(__dirname, "../build/wbg_ristretto/index.js"),
-  entryModule
-);
-writeFileSync(path.join(__dirname, "../build/wbg_p256/index.js"), entryModule);
+const packageJson = function (name) {
+  return new sh.ShellString(`{
+  "name": "@serenity-kit/${name}",
+  "collaborators": [
+    "Stefan Oestreicher <oestef@gmail.com>",
+    "Nik Graf <nik@nikgraf.com>"
+  ],
+  "version": "${rootPkg.version}",
+  "license": "MIT",
+  "files": [
+    "index.d.ts",
+    "esm/index.js",
+    "cjs/index.js"
+  ],
+  "module": "esm/index.js",
+  "types": "index.d.ts",
+  "main": "cjs/index.js",
+  "browser": "esm/index.js"
+}`);
+};
+
+function build_wbg() {
+  sh.exec(
+    "cargo build --target=wasm32-unknown-unknown --release --features wee_alloc"
+  );
+  sh.exec(
+    "wasm-bindgen --out-dir=build/wbg_ristretto --target=web --omit-default-module-path target/wasm32-unknown-unknown/release/opaque.wasm"
+  );
+  sh.exec(
+    "cargo build --target=wasm32-unknown-unknown --release --features wee_alloc,p256"
+  );
+  sh.exec(
+    "wasm-bindgen --out-dir=build/wbg_p256 --target=web --omit-default-module-path target/wasm32-unknown-unknown/release/opaque.wasm"
+  );
+}
+
+function bundle(name) {
+  sh.exec("pnpm rollup -c", {
+    env: {
+      ...process.env,
+      BUILD_ENTRY: name,
+    },
+  });
+}
+
+function main() {
+  sh.rm("-rf", "build");
+
+  // build rust code and generate wasm bindings
+  build_wbg();
+
+  // write entry module for intermediate builds
+  entryModule.to("build/wbg_ristretto/index.js");
+  entryModule.to("build/wbg_p256/index.js");
+
+  // rollup
+  bundle("ristretto");
+  bundle("p256");
+
+  // write package json
+  packageJson("opaque").to("build/ristretto/package.json");
+  packageJson("opaque-p256").to("build/p256/package.json");
+
+  // copy docs
+  sh.cp("README.md", "build/ristretto/README.md");
+  sh.cp("README.md", "build/p256/README.md");
+  sh.cp("LICENSE", "build/ristretto/LICENSE");
+  sh.cp("LICENSE", "build/p256/LICENSE");
+
+  // copy type defs
+  sh.cp("build/wbg_ristretto/opaque.d.ts", "build/ristretto/index.d.ts");
+  sh.cp("build/wbg_p256/opaque.d.ts", "build/p256/index.d.ts");
+
+  // amend type defs
+  const ts = new sh.ShellString("export const ready: Promise<void>;");
+  ts.toEnd("build/ristretto/index.d.ts");
+  ts.toEnd("build/p256/index.d.ts");
+}
+
+main();
