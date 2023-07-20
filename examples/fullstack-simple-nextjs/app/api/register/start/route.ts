255ca84189da6147e7bc1ebcdef2fda325d873ab
Stefan Oestreicher <oestef@gmail.com>
2023-07-20 17:36:23 +0200

    Add redis support to nextjs fullstack example

--- examples/fullstack-simple-nextjs/app/api/register/start/route.ts
+++ examples/fullstack-simple-nextjs/app/api/register/start/route.ts
@@ -1,6 +1,7 @@
 import * as opaque from "@serenity-kit/opaque";
 import { NextRequest, NextResponse } from "next/server";
 import database from "../../db";
+import { SERVER_SETUP } from "../../env";
 
 export async function POST(req: NextRequest) {
   const { userIdentifier, registrationRequest } = await req.json();
@@ -17,15 +18,15 @@ export async function POST(req: NextRequest) {
     );
 
   const db = await database;
-
-  if (db.hasUser(userIdentifier))
+  const hasUser = await db.hasUser(userIdentifier);
+  if (hasUser)
     return NextResponse.json(
       { error: "user already registered" },
       { status: 400 }
     );
 
   const { registrationResponse } = opaque.server.createRegistrationResponse({
-    serverSetup: db.getServerSetup(),
+    serverSetup: SERVER_SETUP,
     userIdentifier,
     registrationRequest,
   });
