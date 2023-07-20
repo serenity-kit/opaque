d15ea27076b86c153cb827bc08e5e5dd815f4831
Nik Graf <nik@nikgraf.com>
2023-07-20 21:00:53 +0200

    add session util

---
+++ examples/fullstack-e2e-encrypted-locker-nextjs/app/utils/session/createAuthorizationToken.ts
@@ -0,0 +1,16 @@
+import sodium from "libsodium-wrappers";
+
+export const createAuthorizationToken = ({
+  sessionKey,
+  subkeyId = 924,
+}: CreateAuthorizationTokenParams) => {
+  const sessionKeyAsUint8Array = sodium.from_base64(sessionKey);
+  const authorizationToken = sodium.crypto_kdf_derive_from_key(
+    sodium.crypto_aead_xchacha20poly1305_ietf_KEYBYTES,
+    subkeyId, // publicly available subkey_id
+    "session",
+    sessionKeyAsUint8Array
+  );
+
+  return sodium.to_base64(authorizationToken);
+};
