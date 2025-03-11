ce81e06572b48755816365e20f72fd4003445a12
Nik Graf <nik@nikgraf.com>
2025-03-11 19:40:53 +0100

    upgrade example dependencies (#134)

--- examples/client-simple-webpack/webpack.config.js
+++ examples/client-simple-webpack/webpack.config.js
@@ -10,11 +10,12 @@ module.exports = {
   mode: "development",
   plugins: [new CopyWebpackPlugin({ patterns: ["index.html"] })],
   devServer: {
-    proxy: {
-      "/api": {
+    proxy: [
+      {
+        context: ["/api"],
         target: "http://localhost:8089",
         pathRewrite: { "^/api": "" },
       },
-    },
+    ],
   },
 };
